c5190339a14d42a910984e5868b13604a8257439
Tyralon <tyraloneu@gmail.com>
2021-10-07 17:17:05 +0200

    added mp5 ticks

--- hpala.py
+++ hpala.py
@@ -2,9 +2,16 @@ t = 0.0
 mana_pool = 10000
 fol_mana = 180
 fol_cast = 1.5
+mp5 = 100
+mana_tick = 2
+mp2 = mp5 / 5 * 2
+last_tick = t
 
 while mana_pool >= fol_mana:
+	while last_tick < t:
+		last_tick += mana_tick
+		mana_pool += mp2
 	mana_pool -= fol_mana
 	t += fol_cast
 
-print(t)
+print('Time to OOM: ' + str(t))
