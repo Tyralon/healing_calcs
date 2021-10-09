d277072d9bb13919a2dd6d11840a0c0a4020c6fd
Tyralon <tyraloneu@gmail.com>
2021-10-09 10:56:26 +0200

    added activity level. also divine illumination misattributed

--- hpala.py
+++ hpala.py
@@ -2,7 +2,7 @@ import random
 import statistics
 
 mana_pool = 10000
-crit = 22.25
+crit = 0.2225
 mp5 = 88
 
 def mana_source(lower, upper, modifier):
@@ -18,9 +18,9 @@ def mana_pot_alch():
 
 # whether we get a crit on our spell
 def spell_crit(crit_percentage):
-	return random.randint(1,10000) < (crit_percentage * 100)
+	return random.random() < crit_percentage
 
-def simulation(mana_pool, mp5, crit):
+def simulation(activity, mana_pool, mp5, crit):
 	t = 0.0
 	fol_mana = 180
 	fol_cast = 1.5
@@ -37,9 +37,9 @@ def simulation(mana_pool, mp5, crit):
 	rune_delay = 60
 	rune_last_use = rune_delay - rune_cd
 
-	div_illu = 0.6
+	illu = 0.6
 
-	while mana_pool >= fol_mana:
+	while mana_pool >= fol_mana and t < 600:
 		while last_tick < t:
 			last_tick += mana_tick
 			mana_pool += mp2
@@ -54,16 +54,18 @@ def simulation(mana_pool, mp5, crit):
 		mana_pool -= fol_mana
 	
 		if spell_crit(crit):
-			mana_pool += fol_mana * div_illu
+			mana_pool += fol_mana * illu
 
 		t += fol_cast
+		# adds delay based on y = -(x-1) / x
+		t += -(activity - 1) / activity * fol_cast
 	return t
 
 
 sims = []
 for i in range(50):
-	sims.append(simulation(mana_pool, mp5, crit))
+	sims.append(simulation(0.9, mana_pool, mp5, crit))
 
-print('TTO min: ' + str(min(sims)))
-print('TTO max: ' + str(max(sims)))
-print('TTO mode: ' + str(statistics.median(sims)))
+print('TTO min: ' + str(round(min(sims))))
+print('TTO max: ' + str(round(max(sims))))
+print('TTO mode: ' + str(round(statistics.median(sims))))
