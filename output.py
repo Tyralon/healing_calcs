022c91c507140931500f50e8f970103cf79fb56d
Tyralon <tyraloneu@gmail.com>
2021-10-23 13:52:26 +0200

    analysis part: percentage increase

--- output.py
+++ output.py
@@ -4,16 +4,23 @@ import numpy as np
 
 def ana_helper(arr, steps):
 	x = np.arange(0, steps)
+	y = np.zeros(steps)
+	for i in range(steps - 1):
+		y[i] = (arr[i+1, 0] - arr[i, 0]) / arr[i, 0]
 	for i in range(steps):
-		print(str(round(arr[i, 0], 2)) + ", " + str(round(arr[i, 1] * 100)) + "% ")
+		if i == 0:
+			print(str(round(arr[i, 0], 2)) + "\t\t" + str(round(arr[i, 1] * 100)) + "% ")
+		else:
+			print(str(round(arr[i, 0], 2)) + "\t" + str(round(y[i-1] * 100, 3)) + "%\t" + str(round(arr[i, 1] * 100)) + "% ")
+
 	z = np.polyfit(x, arr[:,0], 1)
 	print("OLS: " + str(round(z[0], 2)))
+	print("Average increase: " + str(round(np.mean(y) * 100, 3)) + "%")
 
 
 def analysis(arr, steps):
 	print("\nincreased healing")
 	ana_helper(arr[0], steps)
-	x = np.arange(0, steps)
 	print("\nincreased mp5")
 	ana_helper(arr[1], steps)
 	print("\nincreased crit")
