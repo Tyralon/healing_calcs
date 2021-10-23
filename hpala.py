299b150e4544fdc336438a99d0e58d32c92b08f5
Tyralon <tyraloneu@gmail.com>
2021-10-23 13:53:18 +0200

    modularized simulation part. a spell as a class

--- hpala.py
+++ hpala.py
@@ -2,21 +2,44 @@ import random
 import statistics
 import numpy as np
 
+class Healing:
+	
+	def __init__(self, lower, upper, cast, mana, healing, crit):
+		self.lower = lower
+		self.upper = upper
+		self.cast = cast
+		self.mana = mana
+		self.healing = healing
+		self.crit = crit
+		self.base_cast = cast
+		self.base_mana = mana
+		self.critted = False
 
-def heal(lower, upper, cast, healing, critted):
-	if critted:
-		return (random.randint(lower, upper) + (healing * cast / 3.5 * 1.12)) * 1.5
-	else:
-		return random.randint(lower, upper) + healing * cast / 3.5 * 1.12
+	def getCast(self):
+		return self.cast
 
-def flash_of_light(healing, critted):
-	return heal(513, 574, 1.5, healing, critted)
+	def updateGrace(self, t, last_use):
+		if (last_use + 15) >= t:
+			self.cast = self.base_cast - 0.5
+		else:
+			self.cast = self.base_cast
+	
+	def setCritted(self, boolean):
+		self.critted = boolean
 
-def holy_light_r9(healing, critted):
-	return heal(1813, 2015, 2.5, healing, critted)
+	def getCritted(self):
+		return self.critted
 
-def holy_light_r11(healing, critted):
-	return heal(2459, 2740, 2.5, healing, critted)
+	def getBaseMana(self):
+		return self.base_mana
+
+	def heal(self):
+		if random.random() < self.crit:
+			self.critted = True
+			return (random.randint(self.lower, self.upper) + (self.healing * self.base_cast / 3.5 * 1.12)) * 1.5
+		else:
+			self.critted = False
+			return (random.randint(self.lower, self.upper) + (self.healing * self.base_cast / 3.5 * 1.12))
 
 def mana_source(lower, upper, modifier):
 	return random.randint(lower,upper) * modifier
@@ -29,20 +52,16 @@ def mana_rune():
 def mana_pot_alch():
 	return mana_source(1800, 3000, 1.4)
 
-# whether we get a crit on our spell
-def spell_crit(crit_percentage):
-	return random.random() < crit_percentage
-
 def encounter(activity, ratio, mana_pool, healing, mp5, base_crit):
 	assert sum(ratio) == 100
 	t = 0.0
 	healed = 0
-
+	
+	fol = Healing(513, 574, 1.5, 180, healing, base_crit)
+	hl9 = Healing(1813, 2015, 2.5, 660, healing, base_crit + 0.06)
+	hl11 = Healing(2459, 2740, 2.5, 840, healing, base_crit + 0.06)
+	
 	fol_mana = 180
-	fol_cast = 1.5
-	hl9_mana = 660
-	hl11_mana = 840
-	hl_cast = 2.5
 
 	grace = 0
 	grace_effect = 0.5
@@ -81,10 +100,12 @@ def encounter(activity, ratio, mana_pool, healing, mp5, base_crit):
 	limit_reached = False
 
 	while mana_pool >= fol_mana and not limit_reached:
+		# adds mana from mp5
 		while last_tick < t:
 			last_tick += mana_tick
 			mana_pool += mp2
 
+		# whether to pot/rune
 		if t > pot_delay and (pot_last_use + pot_cd) <= t:
 			mana_pool += mana_pot_alch()
 			pot_last_use = t
@@ -92,71 +113,57 @@ def encounter(activity, ratio, mana_pool, healing, mp5, base_crit):
 			mana_pool += mana_rune()
 			rune_last_use = t
 		
+		# which heal/rank to cast
 		rvar = random.random()
 		if rvar < fol_ratio:
-			crit = base_crit
-			spell_mana = fol_mana
-			spell_cast = fol_cast
+			spell = fol
 		elif rvar < hl9_ratio:
-			crit = base_crit + 0.06
-			spell_mana = hl9_mana
-			if (grace_last_use + grace_duration) >= t:
-				spell_cast = hl_cast - grace_effect
-			else:
-				spell_cast = hl_cast
+			spell = hl9
+			spell.updateGrace(t, grace_last_use)
 			grace = 1
 		else:
-			crit = base_crit + 0.06
-			spell_mana = hl11_mana
-			if (grace_last_use + grace_duration) >= t:
-				spell_cast = hl_cast - grace_effect
-			else:
-				spell_cast = hl_cast
+			spell = hl11
+			spell.updateGrace(t, grace_last_use)
 			grace = 1
 			
-
+		# whether to pop cooldowns
 		if t > favor_delay and (favor_last_use + favor_cd) <= t:
+			spell.setCritted(True)
 			favor = 1
-			crit = 1.0
-
 		if t > div_illu_delay and (div_illu_last_use + div_illu_cd) <= t:
 			div_illu_last_use = t
 
-		t += spell_cast
+
+		t += spell.getCast()
+
+		# casts the spell and adds it to the total healing
+		healed += spell.heal()
+		
+		# removes/adds mana from mana pool
 		if (div_illu_last_use + div_illu_duration) >= t:
-			mana_pool -= spell_mana / 2
+			mana_pool -= spell.getBaseMana() / 2
 		else:
-			mana_pool -= spell_mana
+			mana_pool -= spell.getBaseMana()
+		if spell.getCritted():
+			mana_pool += spell.getBaseMana() * illu
 
-		if spell_crit(crit):
-			mana_pool += spell_mana * illu
-			if spell_mana == 180:
-				healed += flash_of_light(healing, True)
-			if spell_mana == 660:
-				healed += holy_light_r9(healing, True)
-			if spell_mana == 840:
-				healed += holy_light_r11(healing, True)
-		else:
-			if spell_mana == 180:
-				healed += flash_of_light(healing, False)
-			if spell_mana == 660:
-				healed += holy_light_r9(healing, False)
-			if spell_mana == 840:
-				healed += holy_light_r11(healing, False)
-
+		# sets DF on CD
 		if favor == 1:
 			favor = 0
 			favor_last_use = t
-
+		# updates last use for light's grace
 		if grace == 1:
 			grace = 0
 			grace_last_use = t
 
-		# adds delay based on y = -(x-1) / x
-		t += -(activity - 1) / activity * spell_cast
+		# randomly adds delay for next cast based on y = -(x-1) / x
+		t += -(activity - 1) / activity * spell.getCast()
+
+		# checks time limit
 		if t >= limit:
 			limit_reached = True
 #			print("Limit reached! - Mana left: " + str(round(mana_pool)))
+
 	return (t, healed, limit_reached)
 
 def simulation(runs, activity, ratio, mana_pool, healing, mp5, crit):
@@ -177,9 +184,9 @@ def simulation(runs, activity, ratio, mana_pool, healing, mp5, crit):
 	return [tto_median, heal_median, hps_median, over_limit / runs]
 
 def gathering_results():
-	runs = 5000
-	activity = 0.9
-	ratio = [75, 20, 5]
+	runs = 50
+	activity = 0.95
+	ratio = [97, 0, 3]
 	mana_pool = 11000
 	crit = 0.1900
 	crit_step = 0.0036
