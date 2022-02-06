22625ec08218b52f88c4beb65c52e75b5fc48849
Tyralon <tyraloneu@gmail.com>
2022-02-06 13:11:16 +0100

    more Pool

--- process_test.py
+++ process_test.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import random
+import time
 from multiprocessing import Process, Lock, Value, Array, Manager, Pool
 
 counter = 0
@@ -23,24 +24,35 @@ def foo_proc(lock, counter, numbers, value, limit):
 		numbers.append(val)
 
 
-
 if __name__ == '__main__':
 
 	mngr = Manager()
 	numbers = mngr.list([])
 	counter = mngr.Value('i', 0)
+	numbers2 = mngr.list([])
+	counter2 = mngr.Value('i', 0)
 	lock = mngr.Lock()
 
 	arg = (lock, counter, numbers, 9, 7,)
+	arg2 = (lock, counter2, numbers2, 9, 3,)
 
 	with Pool(2) as pool:
+		pool.apply_async(time.sleep, [5])
 		pool.starmap(foo_proc, [arg, arg])
 
-	pool.close()
-	pool.join()
+		print(numbers[:])
+		print(counter.value)
 
-	print(numbers[:])
-	print(counter.value)
+#	numbers.clear()
+#	counter.value = 0
+
+		pool.starmap(foo_proc, [arg2, arg2])
+
+		pool.close()
+		pool.join()
+
+		print(numbers2[:])
+		print(counter2.value)
 
 #	counter = 0
 #	numbers = []
